660fa35dd2637350eca73a976ee5ca6af87424de
Joanne <lee.joanne54@gmail.com>
2022-06-23 19:30:12 +0000

    Create models and admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Comments
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email_address', 'body')
+    actions = ['approve_comments']
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
